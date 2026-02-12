2c3c16f8bb569c426d8d40acf9f25f1c7da4c8b4
HKtransport <transporthk92@gmail.com>
2026-02-12 20:54:58 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,41 @@
-// Change le nom du cache à chaque grosse mise à jour
-const CACHE_NAME = "baba-colis-cache-v3";
-
+const CACHE_NAME = "baba-colis-cache-v1";
 const urlsToCache = [
-  "index.html",
-  "envoyer.html",
-  "paiement.html",
-  "confirmation.html",
-  "suivre.html",
-  "pointsrelais.html",
-  "contact.html",
-  "success.html",
-  "manifest.json",
-  "icon-192.png",
-  "icon-512.png"
+  "/",
+  "/index.html",
+  "/envoyer.html",
+  "/suivre.html",
+  "/pointsrelais.html",
+  "/contact.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
 ];
 
+// Install
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
+// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
+  );
+});
+
+// Update
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
   );
 });
