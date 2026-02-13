9842992fa119db2eab4ecc82ac580b502e029b28
HKtransport <transporthk92@gmail.com>
2026-02-13 15:40:48 +0100

    Update paytech.php

--- paytech.php
+++ paytech.php
@@ -1,8 +1,8 @@
 <?php
 
 // ⚠️ METS TES VRAIES CLÉS ICI
-$api_key = "VOTRE_API_KEY";
-$secret_key = "VOTRE_SECRET_KEY";
+$api_key = "d9f2c4c89d05033ac5d3b1cbaa9eba42fcefc7d9dce1338ebdf45589c96b5b4c";
+$secret_key = "6058a1e780aa99e4cd411b435c0bbd3f1299557ed9dc120d15b1cc8be41c3d9c";
 
 // Récupération des données envoyées depuis paiement.html
 $prix = $_POST["prix"];
